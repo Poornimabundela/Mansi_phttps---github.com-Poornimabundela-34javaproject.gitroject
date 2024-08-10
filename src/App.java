63d2b449a7a7b82c3f1002e99ee56f29eb2b8f9a
unknown <Administrator@DESKTOP-B1IQIN7.magnumswg.local>
2024-08-10 21:50:21 +0530

    meeage

---
+++ src/App.java
@@ -0,0 +1,8 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        // Launch the Calculator application
+        javax.swing.SwingUtilities.invokeLater(() -> new Calculator());
+    }
+}
